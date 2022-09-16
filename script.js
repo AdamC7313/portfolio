434221598e76ed9369953682b7378de4a81aa93e
AdamC7313 <AdamC7313@gmail.com>
2022-09-16 12:28:11 -0400

    add button functionality in JS

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-let homeContent = document.getElementsByClassName('home');
-let projectsContent = document.getElementsByClassName('projects');
-let aboutmeContent = document.getElementsByClassName('about-me');
-let skillsContent = document.getElementsByClassName('skills');
+let homeButton = document.getElementById("home-button")
+let aboutmeButton = document.getElementById("aboutme-button")
+let projectsButton = document.getElementById("projects-button")
+let skillsButton = document.getElementById("skills-button")
+
+let homeContent = document.getElementsByClassName("content")[0]
+let aboutmeContent = document.getElementsByClassName("content")[1]
+let projectsContent = document.getElementsByClassName("content")[2]
+let skillsContent = document.getElementsByClassName("content")[3]
 
 homeContent.hidden = false;
-projectsContent.hidden = true;
 aboutmeContent.hidden = true;
+projectsContent.hidden = true;
 skillsContent.hidden = true;
 
-homeContent.onclick = function() {
+homeButton.onclick = function() {
     homeContent.hidden = false;
-    projectsContent.hidden = true;
     aboutmeContent.hidden = true;
+    projectsContent.hidden = true;
     skillsContent.hidden = true;
 }
 
-aboutmeContent.onclick = function() {
+aboutmeButton.onclick = function() {
     homeContent.hidden = true;
-    projectsContent.hidden = true;
     aboutmeContent.hidden = false;
-    skillsContent.hidden = true;
-}
-
-projectsContent.onclick = function() {
-    homeContent.hidden = true;
-    projectsContent.hidden = false;
-    aboutmeContent.hidden = true;
-    skillsContent.hidden = true;
-}
-
-skillsContent.onclick = function() {
-    homeContent.hidden = true;
     projectsContent.hidden = true;
+    skillsContent.hidden = true;
+}
+
+projectsButton.onclick = function() {
+    homeContent.hidden = true;
     aboutmeContent.hidden = true;
+    projectsContent.hidden = false;
+    skillsContent.hidden = true;
+}
+
+skillsButton.onclick = function() {
+    homeContent.hidden = true;
+    aboutmeContent.hidden = true;
+    projectsContent.hidden = true;
     skillsContent.hidden = false;
 }
