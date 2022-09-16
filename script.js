4625cecdbd2b582a0acbcdbfed2969c0dc382cdb
AdamC7313 <AdamC7313@gmail.com>
2022-09-16 15:54:51 -0400

    add contact button and text hovering on icons

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ let homeButton = document.getElementById("home-button")
 let aboutmeButton = document.getElementById("aboutme-button")
 let projectsButton = document.getElementById("projects-button")
 let skillsButton = document.getElementById("skills-button")
+let linkedinButton = document.getElementsByClassName("contact-button")[0]
+let emailButton = document.getElementsByClassName("contact-button")[1]
 
 let homeContent = document.getElementsByClassName("content")[0]
 let aboutmeContent = document.getElementsByClassName("content")[1]
 let projectsContent = document.getElementsByClassName("content")[2]
 let skillsContent = document.getElementsByClassName("content")[3]
+let linkedinText = document.getElementById("linkedin")
+let emailText = document.getElementById("email")
+
 
 homeContent.hidden = false;
 aboutmeContent.hidden = true;
 projectsContent.hidden = true;
 skillsContent.hidden = true;
+linkedinText.hidden = true;
+emailText.hidden = true;
 
 homeButton.onclick = function() {
     homeContent.hidden = false;
@@ -39,4 +46,20 @@ skillsButton.onclick = function() {
     aboutmeContent.hidden = true;
     projectsContent.hidden = true;
     skillsContent.hidden = false;
+}
+
+linkedinButton.onmouseover = function() {
+    linkedinText.hidden = false;
+}
+
+linkedinButton.onmouseout = function() {
+    linkedinText.hidden = true;
+}
+
+emailButton.onmouseover = function() {
+    emailText.hidden = false;
+}
+
+emailButton.onmouseout = function() {
+    emailText.hidden = true;
 }
